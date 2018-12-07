28301412dbc5ee8c8668fc35618ef72b58969e5c
dc179 <dc179@csserver.evansville.edu>
2018-12-06 23:37:20 -0600

    Final v2.6

--- Final/gui-player.cpp
+++ Final/gui-player.cpp
@@ -11,21 +11,57 @@
 #include <vector>
 #include <algorithm>
 
+#include <gtk/gtk.h>
+
 using namespace std;
+
+static void print_hello (GtkWidget *widget,
+             gpointer   data)
+{
+  g_print ("Hello World\n");
+}
+
+static void activate (GtkApplication *app,
+          gpointer        user_data)
+{
+  GtkWidget *window;
+  GtkWidget *button;
+  GtkWidget *button_box;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+  button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+  gtk_container_add (GTK_CONTAINER (window), button_box);
+
+  button = gtk_button_new_with_label ("Hello World");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+  gtk_container_add (GTK_CONTAINER (button_box), button);
+
+  gtk_widget_show_all (window);
+}
+
 
 int main(int argc, char *argv[])
 {
+/*
     const char* moveA[] = {"rock", "paper", "scissors", "r", "p", "s"};
     const vector<string> move(moveA, end(moveA));
     int sockfd;
     int len;
     struct addrinfo *r;
     int nread, result;
+
+    cout<<"Flag1"<<endl;
     
     if(argc != 3) {
         cerr << "syntax: shout [server] [port]" << endl;
         exit(1);
     }
+
+    cout<<"Flag2"<<endl;
     
     getaddrinfo(argv[1], argv[2], NULL, &r);
     // Create a socket for the client.
@@ -36,8 +72,28 @@ int main(int argc, char *argv[])
         cerr << "client: connect failed" << endl;
         exit(1);
     }
-    
+
+    cout<<"Flag3"<<endl;
+*/
+
+
+    GtkApplication *app;
+    int stat;
+
+    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    stat = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
+
+
+
+  
+    return stat;
+
+
+/*  
     // We can now read/write via sockfd.
+    // GAME ON ////////////////////////
     string input, response;
     static char buffer[128];
     int buflen = sizeof(buffer);
@@ -97,4 +153,6 @@ int main(int argc, char *argv[])
     cout << endl;
     close(sockfd);
     exit(0);
+*/
+
 }
